9bb17b127274a23d97b879472cd395dc8bff93c0
GitHub Action <action@github.com>
2023-10-25 08:00:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/biopy3/massideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/biopy3/massideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.biopy3.massideaplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/biopy3/massideaplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/biopy3/massideaplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.biopy3.massideaplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
